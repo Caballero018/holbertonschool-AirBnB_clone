425a1480fc6a4a65ef52a3f49540ca5e2abdc3fb
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-25 17:16:07 -0500

    Class BaseModel that define kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     '''Base class to string and datas'''
     def __init__(self, *args, **kwargs):
         '''initialization of datas od dictionary's'''
-        if kwargs is not None or kwargs:
+        if kwargs is not None or kwargs or (kwargs is not None and kwargs):
             self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
