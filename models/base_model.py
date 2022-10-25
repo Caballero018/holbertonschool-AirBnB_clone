293f55785ac861c61f7d8e8c4d1676bf132e7c98
Jehp00 <Jehp01104006@gmail.com>
2022-10-25 18:03:41 -0500

    base model/attributes of id and date

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,13 @@ from datetime import datetime
 
 class BaseModel:
     '''Base class to string and datas'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''initialization of datas od dictionary's'''
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-    
+
     def __str__(self):
         '''str'''
         return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
