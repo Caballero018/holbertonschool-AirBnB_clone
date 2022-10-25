8d2bb3ea5e411d119f45c17d36027e7e18585422
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-25 16:38:31 -0500

    Class BaseModel that define kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -11,16 +11,18 @@ from datetime import datetime
 
 class BaseModel:
     '''Base class to string and datas'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''initialization of datas od dictionary's'''
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if kwargs is not None:
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
-    
     def __str__(self):
         '''str'''
-        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(
+                type(self).__name__, self.id, self.__dict__
+                ))
 
     def save(self):
         '''save method to updates the attribute updated_at'''
