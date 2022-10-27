fb534eaa62f3e3cc6edccaae499f2e4517b8701c
Jehp00 <Jehp01104006@gmail.com>
2022-10-27 11:46:43 -0500

    create with messege error

--- console.py
+++ console.py
@@ -26,10 +26,16 @@ class HBNBCommand(cmd.Cmd, BaseModel):
 
 
     def do_create(self, inp):
-        if inp == "BaseModel":
-            inp = BaseModel()
-            print(inp.id)
-
+        if inp:
+            try:
+                inp = inp + "()"
+                inp = eval(inp)
+                print(inp.id)
+            except NameError:
+                print("** class doesn't exist **")
+            inp.save()
+        else:
+            print("** class name missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
