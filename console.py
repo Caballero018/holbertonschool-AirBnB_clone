03862dc63b96e8e5a3fcfbed970d30245d8e4aab
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-26 19:37:10 -0500

    Program called console.py that contains the entry point of the command interpreter

--- console.py
+++ console.py
@@ -6,7 +6,22 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     "Doc"
     prompt = "(hbnb) "
-    
+
+    def do_quit(self, inp):
+        "Doc"
+        return True
+
+    def help_quit(self):
+        "Doc"
+        print("Quit command to exit the program")
+
+    def do_EOF(self, inp):
+        "Doc"
+        return True
+
+    def help_EOF(self):
+        "Doc"
+        print("Quit command to exit the program")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
