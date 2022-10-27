a46c19b84aecfca22f42a60736a0468a42d3b81c
Jehp00 <Jehp01104006@gmail.com>
2022-10-27 11:13:04 -0500

    create command

--- console.py
+++ console.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 "Doc"
 import cmd
+from models.base_model import BaseModel
 
 
-class HBNBCommand(cmd.Cmd):
+class HBNBCommand(cmd.Cmd, BaseModel):
     "Doc"
     prompt = "(hbnb) "
 
@@ -18,13 +19,16 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, inp):
         "Doc"
         return True
-    
-    def do (sef, inp):
-        return
 
     def help_EOF(self):
         "Doc"
         print("Quit command to exit the program")
+
+    def do_create(self, inp):
+        if inp == "BaseModel":
+            inp = BaseModel()
+            print(inp.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
